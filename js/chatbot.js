7947401976ec19190801c1a1a86be7a5bfbdaf2c
woobin <whdnqls95@gmail.com>
2022-12-09 18:09:41 +0900

    form 비활성화 이벤트 수정 및 페이지 추가

--- js/chatbot.js
+++ js/chatbot.js
@@ -13,19 +13,47 @@ var fn = (function() {
 			//fromFocus
 			fn.fromFocus();
 
+			//btnlist
+			fn.btnlist();
+
 			//tab
 			fn.tab();
 		},
 		
 		//fromFocus
 		fromFocus : function(){
-			$(document).on("focus blur change", ".form_item input", function(){
-				var $this = $(this);
+			//value 유무에 따른 활성화/비활성화
+			function formValuCheck($this) {
 				if($this.val().trim() == ""){
 					$this.closest(".form_item").removeClass("is_complete");
 				}else {
 					$this.closest(".form_item").addClass("is_complete");
 				}
+			}
+
+			//value 유무 체크 : 초기 default
+			$(".form_item input").each(function(){
+				var $this = $(this);
+				if($this.is(':radio') || $this.is(':checkbox')) {
+					if($this.is(':checked')){
+						$this.closest(".form_item").addClass("is_complete");
+					}
+				}else{
+					formValuCheck($this);
+				}
+			});
+
+			//value 유무 체크 : 동작 event
+			$(document).on("focus blur change", ".form_item input, .form_item textarea", function(){
+				formValuCheck($(this));
+			});
+		},
+		
+		//btnlist
+		btnlist : function(){
+			$(document).on("click", ".btn_list .item", function(){
+				var $this = $(this);
+				$this.addClass("is_active").siblings().removeClass("is_active");
 			});
 		},
 
@@ -34,8 +62,8 @@ var fn = (function() {
 			$(document).on("click", ".tab-menu a", function() {
 				var $this = $(this);
 				var index = $this.index();
-				$this.addClass("active").siblings().removeClass("active");
-				$(".tab-cont > div").eq(index).addClass("active").siblings().removeClass("active");
+				$this.addClass("is_active").siblings().removeClass("is_active");
+				$(".tab-cont > div").eq(index).addClass("is_active").siblings().removeClass("is_active");
 			});
 		},
 
