de486cd2d47b099b59a1392611569ee3704145a0
woobin <whdnqls95@gmail.com>
2022-12-13 16:31:13 +0900

    필요서류 안내 퍼블 작업, js파일명 변경

--- js/ui.chatbot.js
+++ js/ui.chatbot.js
@@ -1,6 +1,6 @@
 /**
 * --------------------------------
-* Common JS
+* ui.chatbot JS
 * creator : chowoobin
 * --------------------------------
 */
@@ -10,14 +10,46 @@ var fn = (function() {
 	return {
 		//공통
 		common : function(){
-			//fromFocus
-			fn.fromFocus();
-
-			//btnlist
-			fn.btnlist();
-
 			//tab
 			fn.tab();
+
+			//btnList
+			fn.btnList();
+
+			//fileDel
+			fn.fileDel();
+
+			//fromFocus
+			fn.fromFocus();
+		},
+
+		//tab
+		tab : function(){
+			$(document).on("click", ".tab_menu input[type='radio']", function() {
+				var $this = $(this),
+						$tabcont = $this.closest(".tab_wrap").find(".tab_item"),
+						index = $this.closest("label").index();
+
+				$tabcont.eq(index).addClass("is_active").siblings().removeClass("is_active");
+			});
+		},
+		
+		//btnList
+		btnList : function(){
+			$(document).on("click", ".btn_list .item", function(){
+				var $this = $(this);
+
+				$this.addClass("is_active").siblings().removeClass("is_active");
+			});
+		},
+		
+		//fileDel
+		fileDel : function(){
+			$(document).on("click", ".file_list .file_del", function(){
+				var $this = $(this);
+
+				$this.closest(".file_item").removeClass("is_active is_many");
+			});
 		},
 		
 		//fromFocus
@@ -35,6 +67,7 @@ var fn = (function() {
 			//value 유무 체크 : 초기 default
 			$(".form_item input").each(function(){
 				var $this = $(this);
+
 				if($this.is(':radio') || $this.is(':checkbox')) {
 					if($this.is(':checked')){
 						$this.closest(".form_item").addClass("is_complete");
@@ -50,31 +83,14 @@ var fn = (function() {
 			});
 
 			//button event에 따른 value 유무 체크
-			$(document).on("focus blur", ".form_item .form_btn", function(){
+			$(document).on("focus blur change", ".form_item .form_btn", function(){
 				var $this = $(this).siblings("input");
+				
 				if($this.val().trim() == ""){
 					$this.closest(".form_item").removeClass("is_complete");
 				}else {
 					$this.closest(".form_item").addClass("is_complete");
 				}
-			});
-		},
-		
-		//btnlist
-		btnlist : function(){
-			$(document).on("click", ".btn_list .item", function(){
-				var $this = $(this);
-				$this.addClass("is_active").siblings().removeClass("is_active");
-			});
-		},
-
-		//tab
-		tab : function(){
-			$(document).on("click", ".tab-menu a", function() {
-				var $this = $(this);
-				var index = $this.index();
-				$this.addClass("is_active").siblings().removeClass("is_active");
-				$(".tab-cont > div").eq(index).addClass("is_active").siblings().removeClass("is_active");
 			});
 		},
 
